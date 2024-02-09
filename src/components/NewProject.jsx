a8c9fb7d48a1b4d99f01b1fd48d6390d1db513c8
Muhammed Yılmaz <mylmz2101@gmail.com>
2024-02-09 16:35:58 +0300

    modal component, forwardRef & useImperativeHandle added

--- src/components/NewProject.jsx
+++ src/components/NewProject.jsx
@@ -1,8 +1,10 @@
 import { useRef } from 'react';
 
 import Input from "./Input";
+import Modal from './Modal';
 
 export default function NewProject({ onAdd }) {
+    const modal = useRef();
     const title = useRef();
     const description = useRef();
     const dueDate = useRef();
@@ -12,6 +14,11 @@ export default function NewProject({ onAdd }) {
         const enteredDescription = description.current.value;
         const enteredDueDate = dueDate.current.value;
 
+        if (enteredTitle.trim() === '' || enteredDescription.trim() === '' || enteredDueDate.trim() === '') {
+            modal.current.open();
+            return;
+        }
+
         onAdd({
             title: enteredTitle,
             description: enteredDescription,
@@ -19,27 +26,37 @@ export default function NewProject({ onAdd }) {
         });
     }
 
-    return (
-        <div className="w-[35rem] mt-16">
-            <menu className="flex items-center justify-end gap-4 my-4">
-                <li>
-                    <button className="text-stone-800 hover:text-stone-950">Cancel</button>
-                </li>
 
-                <li>
-                    <button
-                        onClick={handleSave}
-                        className=" px-6 py-2 rounded-md bg-stone-800 text-stone-50 hover:bg-stone-950"
-                    >
-                        Save
-                    </button>
-                </li>
-            </menu>
-            <div>
-                <Input type="text" ref={title} label="Title" />
-                <Input ref={description} label="Description" textarea />
-                <Input type="date" ref={dueDate} label="Due Date" />
+
+    return (
+
+        <>
+            <Modal ref={modal} buttonCaption="OK">
+                <h2>Invalid Input</h2>
+                <p>Oops... Looks like you forgot to enter a value.</p>
+                <p>Please fill every single input field.</p>
+            </Modal>
+            <div className="w-[35rem] mt-16">
+                <menu className="flex items-center justify-end gap-4 my-4">
+                    <li>
+                        <button className="text-stone-800 hover:text-stone-950">Cancel</button>
+                    </li>
+
+                    <li>
+                        <button
+                            onClick={handleSave}
+                            className=" px-6 py-2 rounded-md bg-stone-800 text-stone-50 hover:bg-stone-950"
+                        >
+                            Save
+                        </button>
+                    </li>
+                </menu>
+                <div>
+                    <Input type="text" ref={title} label="Title" />
+                    <Input ref={description} label="Description" textarea />
+                    <Input type="date" ref={dueDate} label="Due Date" />
+                </div>
             </div>
-        </div>
+        </>
     );
 }
