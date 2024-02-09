82850dff02e7d948828fb55048b2abd0bfa2b4fd
Muhammed Yılmaz <mylmz2101@gmail.com>
2024-02-09 14:23:53 +0300

    noprojectselected & button component added

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import Sidebar from "./components/SideBar";
 import NewProject from "./components/NewProject";
+import NoProjectSelected from "./components/NoProjectSelected";
 
 function App() {
   return (
     <main className="h-screen my-8 flex gap-8">
       <Sidebar />
-      <NewProject />
+      {/* <NewProject /> */}
+      <NoProjectSelected></NoProjectSelected>
     </main>
   );
 }
