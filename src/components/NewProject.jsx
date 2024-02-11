7b6b93af4c11567a02d8fb0639241568ed2c46a3
Muhammed Yılmaz <mylmz2101@gmail.com>
2024-02-11 12:09:32 +0300

    Modal comğonent styled & cancel button improved

--- src/components/NewProject.jsx
+++ src/components/NewProject.jsx
@@ -3,7 +3,7 @@ import { useRef } from 'react';
 import Input from "./Input";
 import Modal from './Modal';
 
-export default function NewProject({ onAdd }) {
+export default function NewProject({ onAdd, onCancel }) {
     const modal = useRef();
     const title = useRef();
     const description = useRef();
@@ -31,15 +31,18 @@ export default function NewProject({ onAdd }) {
     return (
 
         <>
-            <Modal ref={modal} buttonCaption="OK">
-                <h2>Invalid Input</h2>
-                <p>Oops... Looks like you forgot to enter a value.</p>
-                <p>Please fill every single input field.</p>
+            <Modal ref={modal} buttonCaption="Okay">
+                <h2 className="text-xl font-bold text-stone-700 my-4">Invalid Input</h2>
+                <p className="text-stone-600 mb-4">Oops... Looks like you forgot to enter a value.</p>
+                <p className="text-stone-600 mb-4">Please fill every single input field.</p>
             </Modal>
             <div className="w-[35rem] mt-16">
                 <menu className="flex items-center justify-end gap-4 my-4">
                     <li>
-                        <button className="text-stone-800 hover:text-stone-950">Cancel</button>
+                        <button onClick={onCancel} className="text-stone-800 hover:text-stone-950"
+                        >
+                            Cancel
+                        </button>
                     </li>
 
                     <li>
